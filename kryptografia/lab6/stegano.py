9537835e7fe9538d762398e4ce85d787f8d212d7
Mbuzak <58588944+Mbuzak@users.noreply.github.com>
2022-06-10 12:51:10 +0200

    New lab6 version
    cover.html - longer file with allows providing more letters in mess.txt
    mess.txt - longer version
    stegano.py - already full working version but bugs may occur

--- kryptografia/lab6/stegano.py
+++ kryptografia/lab6/stegano.py
@@ -1,8 +1,11 @@
 # Autor: Marian Buzak
-# Data: 05.06.2022
+# Data: 09.06.2022
 # python 3
 
 import sys
+
+attrs = [['margin-bottom: 0cm;', 'margin-botom: 0cm;'],
+         ['line-height: 100%;', 'lineheight: 100%;']]
 
 
 def bin_form():
@@ -47,6 +50,43 @@ def read_lines(filename):
     return data
 
 
+def prepare_html():
+    lines = read_lines('cover.html')
+    ret = ''
+
+    for line in lines:
+        # 0. <P> -> <p>
+        line = line.replace('<P', '<p')
+        line = line.replace('</P', '</p')
+
+        # 1. Remove spaces at the end of each line
+        # 2. Remove each multi-space
+        line = line[0:len(line) - len(line.lstrip())] + ' '.join(line.split())
+
+        # 3. Remove wrong named attributes which we provide
+        line = line.replace(attrs[0][1], attrs[0][0])  # first word
+        line = line.replace(attrs[1][1], attrs[1][0])  # second word
+
+        # 4. Remove unused <p></p>
+        line = line.replace('<p></p>', '')
+
+        if len(line) > 1:
+            ret += line + '\n'
+
+    return ret
+
+
+def remove(text):
+    while True:
+        if len(text) < 1:
+            return text + ''
+        if text[-1] == '0':
+            text = text[:-1]
+        else:
+            break
+    return text
+
+
 def hide(option):
     def first():
         cover = read_lines('cover.html')
@@ -68,7 +108,7 @@ def hide(option):
         return cover
 
     def second():
-        # is enough spaces?
+        # is enough space?
         cover = read('cover.html')
         cover_spaces = len([i for i in cover if i == ' '])
         #print(cover_spaces)
@@ -97,9 +137,9 @@ def hide(option):
         cover = read('cover.html')
         
         size = 0
-        start, end = '<P', '>'
+        start, end = '<p', '>'
         for i in range(len(cover) - 2):
-            if cover[i:i+2] == '<P':
+            if cover[i:i+2] == '<p':
                 size += 1
         
         size *= 2  # attribute with 2 arguments
@@ -125,8 +165,10 @@ def hide(option):
                 
                 for j in range(len(valid_attr)):
                    
-                    if valid_attr[j] in cover[i+2 : q]:
-                        pass
+                    if valid_attr[j] in cover[i+2:q]:
+                        if binary[p] == '1':
+                            cover[i + 2:q].replace(attrs[0][0], attrs[0][1])
+                            cover[i + 2:q].replace(attrs[1][0], attrs[1][1])
                     else:
                         if p < len(binary):
                             if binary[p] == '0':
@@ -149,7 +191,7 @@ def hide(option):
         cover = read('cover.html')
 
         # can be saved
-        marker, size, p = '</P>', 0, 0
+        marker, size, p = '</p>', 0, 0
         while p < len(cover) - len(marker):
             if cover[p:p + len(marker)] == marker:
                 size += 1
@@ -167,26 +209,30 @@ def hide(option):
         while p < bits_count:
             if cover[i:i + len(marker)] == marker:
                 if binary[p] == '1':
-                    str += '</P><P>'
+                    str += '</p><p></p>'
+                    i += 4
                 else:
-                    str += '</P>'
+                    str += '</p>'
+                    i += 4
                 p += 1
-                i += 4
                 continue
             str += cover[i]
             i += 1
 
-        str += cover[i+1:len(cover)]
+        str += cover[i:len(cover)]
         return str
+
+    # Each function do:
+    # 1. Check is enough space to save message
+    # 2. Hide message and return it
 
     binary = bin_form()
     bits_count = len(binary)
 
-    """
-    file = open('cover.html')
-    cover = file.readlines()
+    text = prepare_html()
+    file = open('cover.html', 'w')
+    file.write(text)
     file.close()
-    """
 
     if option == '-1':
         new = first()
@@ -198,6 +244,7 @@ def hide(option):
         new = fourth()
     else:
         print('Błędny drugi parametr, wybierz [-1, -2, -3, -4]')
+        new = -1
 
     if new != -1:
         file = open('watermark.html', 'w')
@@ -212,8 +259,8 @@ def find(option):
         msg = ''
         watermark = read_lines('watermark.html')
         for i in range(len(watermark)):
-            if i >= msg_len:
-                break
+            #if i >= msg_len:
+            #    break
 
             watermark[i] = watermark[i][:-1]
 
@@ -228,7 +275,8 @@ def find(option):
         msg = ''
         watermark = read('watermark.html')
         p, k = 0, 0
-        while k < msg_len:
+        #while k < msg_len:
+        while p < len(watermark) - 1:
             if watermark[p] == ' ':
                 if watermark[p+1] == ' ':
                     msg += '1'
@@ -243,53 +291,62 @@ def find(option):
         msg = ''
         watermark = read('watermark.html')
         p, i, q = 0, 0, 0
-        start, end = '<P', '>'
+        start, end = '<p', '>'
         attr = {'margin-bottom: 0cm;': '0', 'line-height: 100%;': '0', 'margin-botom: 0cm;': '1', 'lineheight: 100%;': '1'}
-        while p < msg_len:
+        #while p < msg_len:
+        while True:
+            if i+4 >= len(watermark):
+                break
+
             if watermark[i:i+2] == start:
                 q = i + 2
                 while watermark[q] != end:
                     q += 1
                 text = watermark[i+2:q]
                 tmp = []
-                print(text, tmp)
+                #print(text, tmp)
                 for obj in attr.keys():
                     #print(obj)
                     if text.find(obj) != -1:
                         tmp.append([text.find(obj), attr[obj]])
                 #print(tmp)
                 tmp = sorted(tmp, key=lambda x: x[0])
-                print(tmp)
+                #print(tmp)
                 for j in range(len(tmp)):
                     msg += tmp[j][1]
                     p += 1
                 i = q + 1
             else:
                 i += 1
-        print(p)
-        print(msg)
+        #print(p)
+        #print(msg)
         
         return msg
 
     def fourth():
         msg = ''
-        marker = '</P>'
+        marker = '</p>'
         watermark = read('watermark.html')
         p, i = 0, 0
-        while p < msg_len:
-            if watermark[i:i+4] == marker:
-                if watermark[i+4:i+7] == '<P>':
+
+        #while p < msg_len:
+        while True:
+            if i + 11 >= len(watermark):
+                break
+            if watermark[i:i+len(marker)] == marker:
+                #print(watermark[i+4:i+11])
+                if watermark[i+4:i+11] == '<p></p>':
                     msg += '1'
+                    i += 11
                 else:
                     msg += '0'
+                    i += 4
+
                 p += 1
-                i += 4
                 continue
             i += 1
 
         return msg
-
-    msg_len = 2 * 4
 
     if option == '-1':
         new = first()
@@ -301,18 +358,19 @@ def find(option):
         new = fourth()
     else:
         print('Błędny drugi parametr, wybierz [-1, -2, -3, -4]')
+        new = -1
 
-    hexadecimal = hex_form(new)
+    if new != -1:
+        new = remove(new)
+        hexadecimal = hex_form(new)
 
-    file = open('detect.txt', 'w')
-    file.write(hexadecimal)
-    file.close()
-    print('Gotowe')
+        file = open('detect.txt', 'w')
+        file.write(hexadecimal)
+        file.close()
+        print('Gotowe')
     
 
-
 if __name__ == '__main__':
-    # print(sys.argv[1])
     try:
         op_1 = sys.argv[1]
         op_2 = sys.argv[2]
